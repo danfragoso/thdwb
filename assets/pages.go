da31200a9f123f87188349df9ac9708dcd84f962
danfragoso <danilo.fragoso@gmail.com>
2020-05-03 18:33:47 -0300

    Updated home

--- assets/pages.go
+++ assets/pages.go
@@ -17,7 +17,7 @@ func HomePage() []byte {
             I started this project just to see if it was really possible for me to build a big project like that and to learn how the web works under the hood.
        </p>
        <p>
-            If you try to access a website it may not really load correctly, the reason is because the layout implementation is pretty lacking at the moment, I got a basic implemetation of a display block and list item. For now there's no flexbox, no inline and no inline-block.
+            If you try to access a website it may not really load correctly, the reason is because the layout implementation is pretty lacking at the moment, I got a basic implemetation of a display block and list item. For now there's no flexbox and no inline-block.
        </p>
         <p>
             There's no image support, no forms and no links. But I'm slowly working on bringing those things over. One thing that somewhat works is inline css via the style tag. For now something like this will work:
@@ -45,9 +45,7 @@ func HomePage() []byte {
        <p>
             You can find more info at
        </p>
-       <div style="color: blue;">
-         https://github.com/danfragoso/thdwb
-       </div>
+       <a style="color: blue;" href="https://github.com/danfragoso/thdwb">https://github.com/danfragoso/thdwb</a>
     </body>
 
     </html> 
